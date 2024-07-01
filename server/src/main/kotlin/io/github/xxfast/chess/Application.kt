6826c69042115b2a5f2ee2ea1fdf229a69ec8cf3
xxfast <isurukusumal36@gmail.com>
2024-07-01 15:57:24 +1000

    Remove shared server port

--- server/src/main/kotlin/io/github/xxfast/chess/Application.kt
+++ server/src/main/kotlin/io/github/xxfast/chess/Application.kt
@@ -1,6 +1,5 @@
 package io.github.xxfast.chess
 
-import SERVER_PORT
 import io.github.xxfast.chess.discovery.DiscoveryApi
 import io.github.xxfast.chess.discovery.DiscoveryService
 import io.github.xxfast.chess.game.GameApi
@@ -16,7 +15,7 @@ import kotlinx.rpc.serialization.json
 import kotlinx.rpc.transport.ktor.server.rpc
 
 fun main() {
-  embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+  embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
     .start(wait = true)
 }
 
